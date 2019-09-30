c286fa6259e1a9a7f8ced90f91d9024b144e3d0c
Lubomir Kavetskiy <lubomir.kavetskiy@rolique.io>
2019-09-30 21:14:08 +0300

    tmp useMemo

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 
-import { useFetch } from "./useHooks";
+//import { useFetch } from "./useHooks";
 import "./App.css";
 
-function computeLongestWord(arr) {
-  if (!arr) {
-    return [];
-  }
+// function computeLongestWord(arr) {
+//   if (!arr) {
+//     return [];
+//   }
 
-  console.log("computing longest word");
+//   console.log("computing longest word");
 
-  let longestWord = "";
+//   let longestWord = "";
 
-  JSON.parse(arr).forEach(({ body }) =>
-    body.split(" ").forEach((word) => {
-      if (word.length > longestWord.length) {
-        longestWord = word;
-      }
-    })
-  );
+//   JSON.parse(arr).forEach(({ body }) =>
+//     body.split(" ").forEach((word) => {
+//       if (word.length > longestWord.length) {
+//         longestWord = word;
+//       }
+//     })
+//   );
 
-  return longestWord;
-}
+//   return longestWord;
+// }
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [userId, setUserId] = useState(0);
 
-  const { data } = useFetch("https://jsonplaceholder.typicode.com/posts");
-  const longestWord = useMemo(() => computeLongestWord(data), [data]);
+  useEffect(() => {
+    console.log("useEffect");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((rsp) => rsp.json())
+      .then((json) => setUserId(json[count].id));
+  }, [count]);
 
+  const newId = useMemo(() => {
+    console.log("useMemo", userId);
+    return userId + 1;
+  }, [userId]);
+
+  //   const { data } = useFetch("https://jsonplaceholder.typicode.com/posts");
+  //   const longestWord = useMemo(() => computeLongestWord(data), [data]);
+  console.log("render");
   return (
     <div>
       <div>count: {count}</div>
       <button onClick={() => setCount(count + 1)}>increment</button>
-      <div>{longestWord}</div>
+      {/* <div>{longestWord}</div> */}
+      <span>{newId}</span>
     </div>
   );
 };
