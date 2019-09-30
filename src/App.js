97bd377a328c766ce60d95844080c61bc1253972
Lubomir Kavetskiy <lubomir.kavetskiy@rolique.io>
2019-09-30 20:16:36 +0300

    react clouser

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React, { useState, useMemo } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Home, About } from "./pages";
-import { UserContext } from "./UserContext";
+import React, { useState, useRef } from "react";
 
 function AppRouter() {
-  const [user, setUser] = useState(null);
+  const [count, setCount] = useState(0);
+  const countRef = useRef();
 
-  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+  countRef.current = count;
+
+  const handleAlert = () => {
+    setTimeout(() => {
+      console.log(countRef.current);
+    }, 3000);
+  };
 
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about/">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <UserContext.Provider value={value}>
-          <Route path="/" exact component={Home} />
-          <Route path="/about/" component={About} />
-        </UserContext.Provider>
-      </div>
-    </Router>
+    <>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        click
+      </button>
+      <br />
+      <span>{count}</span>
+      <button onClick={handleAlert}>alert</button>
+      <br />
+    </>
   );
 }
 
